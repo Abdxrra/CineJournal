b5a76e94e3df6352c9e92df3f841892ba2ba5640
Abder <Abderra2002@hotmail.com>
2023-10-06 10:02:28 -0400

    set action bar de à propos

--- CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/AProposActivity.kt
+++ CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/AProposActivity.kt
@@ -2,10 +2,16 @@ package ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class AProposActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apropos)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener{ finish() }
     }
 }
