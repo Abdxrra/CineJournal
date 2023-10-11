e4d6220ff80638dc59a1254fb4ff306319ffcccc
Salim <2063054@bdeb.qc.ca>
2023-10-11 14:41:39 -0400

    ajouter intent dans ajout film

--- CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/AjouterEditerFilm.kt
+++ CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/AjouterEditerFilm.kt
@@ -1,14 +1,35 @@
 package ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.net.toUri
+import ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef.EXTRA_TITRE
+import ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef.EXTRA_SLOGAN
+import ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef.EXTRA_ANNEE
+import ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef.EXTRA_NOTE
+import ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef.EXTRA_IMAGE
+
 
 class AjouterEditerFilm : AppCompatActivity() {
 
     lateinit var modeActivity: TextView
+    lateinit var editTitre: EditText
+    lateinit var editSlogan: EditText
+    lateinit var editAnnee: EditText
+    lateinit var editFilmRating: RatingBar
+    lateinit var editFilmImage: ImageView
+    var uriFilm: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ajout_edit_film)
@@ -20,6 +41,11 @@ class AjouterEditerFilm : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
         modeActivity = findViewById(R.id.modeActivity)
+        editTitre = findViewById(R.id.editTitre)
+        editSlogan = findViewById(R.id.editSlogan)
+        editAnnee = findViewById(R.id.editAnnee)
+        editFilmRating = findViewById(R.id.editFilmRating)
+        editFilmImage = findViewById(R.id.editFilmImage)
 
         val extras = intent.extras
         if (extras != null) {
@@ -30,12 +56,12 @@ class AjouterEditerFilm : AppCompatActivity() {
             }
 
             if(extras.containsKey("Titre")){
-                var titre: String? = extras.getString("titre")
-                var slogan: String? = extras.getString("slogan")
-                var annee: String? = extras.getString("annee")
-                var note: String? = extras.getString("note")
-                var image: String? = extras.getString("image")
-
+                editTitre.setText(extras.getString(EXTRA_TITRE))
+                editSlogan.setText(extras.getString(EXTRA_SLOGAN))
+                editAnnee.setText(extras.getString(EXTRA_ANNEE))
+                editFilmRating.rating = (extras.getString(EXTRA_NOTE)!!.toFloat())
+                uriFilm = extras.getString(EXTRA_IMAGE)!!.toString()
+                editFilmImage.setImageURI(uriFilm.toUri())
             }
         }
 
@@ -45,6 +71,14 @@ class AjouterEditerFilm : AppCompatActivity() {
         return true
     }
     fun sauvegarder(){
-        val intent: Intent = 
+
+        val intentMsg = Intent()
+        intentMsg.putExtra(EXTRA_TITRE, editTitre.text)
+        intentMsg.putExtra(EXTRA_SLOGAN, editSlogan.text)
+        intentMsg.putExtra(EXTRA_ANNEE, editAnnee.text)
+        intentMsg.putExtra(EXTRA_NOTE, editFilmRating.rating)
+        intentMsg.putExtra(EXTRA_IMAGE, uriFilm)
+        setResult(RESULT_OK, intentMsg)
+        finish()
     }
 }
--- CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/MainActivity.kt
+++ CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/MainActivity.kt
@@ -1,5 +1,6 @@
 package ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef
 
+import android.app.Activity
 import android.content.ClipData.Item
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
@@ -22,6 +25,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.ArrayList
+
+const val EXTRA_MODE = "ca.qc.bdeb.c5gm.cinejournal.EXTRA_MODE"
+const val EXTRA_TITRE = "ca.qc.bdeb.c5gm.cinejournal.EXTRA_TITRE"
+const val EXTRA_SLOGAN = "ca.qc.bdeb.c5gm.cinejournal.EXTRA_SLOGAN"
+const val EXTRA_ANNEE = "ca.qc.bdeb.c5gm.cinejournal.EXTRA_ANNEE"
+const val EXTRA_NOTE = "ca.qc.bdeb.c5gm.cinejournal.EXTRA_NOTE"
+const val EXTRA_IMAGE = "ca.qc.bdeb.c5gm.cinejournal.EXTRA_IMAGE"
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,13 +63,31 @@ class MainActivity : AppCompatActivity() {
         adapteur = AdapteurListeFilm(applicationContext, this, listeFilmss)
         recyclerView.adapter = adapteur
 
+        val activityAjouter = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data?:Intent()
+                val extras = intent.extras
+                if (extras != null) {
+                    // faire la fonction pour ajouter un film
+                }
+            }
+        }
+
         var ajouterBtn: Button = findViewById(R.id.ajouter)
         ajouterBtn.setOnClickListener{
             val intent = Intent(applicationContext, AjouterEditerFilm::class.java)
-            intent.putExtra("Mode", "Ajouter");
-            startActivity(intent)
+            intent.putExtra(EXTRA_MODE, "Ajouter");
+            activityAjouter.launch(intent)
         }
 
+    }
+    fun ajouterFilm(v: View){
+
+
+
+        //startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -96,11 +125,6 @@ class MainActivity : AppCompatActivity() {
     suspend fun getClient() = withContext(Dispatchers.IO) {
         val dao = AppDatabase.getDatabase(applicationContext).clientDao()
         dao.findByName("abder", "katan")
-    }
-
-    fun ajouterFilm() {
-        // ajouter code pour ajouter un film dans la bd et la liste
-        updateRecyclerView()
     }
 
     fun updateRecyclerView(){
