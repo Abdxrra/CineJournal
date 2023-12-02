641140de270b146aaf34c4a78c3ef664f971786c
Abdxrra <78387960+Abdxrra@users.noreply.github.com>
2023-12-02 00:23:03 -0500

    Update RechercheFragment.kt

--- CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/RechercheFragment.kt
+++ CineJournal/app/src/main/java/ca/qc/bdeb/c5gm/cinejournal/katanbenyoussef/RechercheFragment.kt
@@ -1,6 +1,7 @@
 package ca.qc.bdeb.c5gm.cinejournal.katanbenyoussef
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -41,15 +42,11 @@ class RechercheFragment : Fragment() {
                         if (response.isSuccessful) {
                             val filmResults = response.body()?.results
                             sharedViewModel.setMovieResults(filmResults)
-                        } else {
-                            // Handle API error
                         }
                     } catch (e: Exception) {
-                        // Handle network error
+                        Log.d("API ERROR", "BYE BYE BEN")
                     }
                 }
-            } else {
-                // Handle empty search term
             }
         }
 
